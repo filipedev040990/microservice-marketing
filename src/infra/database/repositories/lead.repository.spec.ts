9c5a71a63c5dcd62153c862b79a9141dc8f4136f
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-18 12:00:31 -0300

    feat: ensure LeadRepository get lead by email

--- src/infra/database/repositories/lead.repository.spec.ts
+++ src/infra/database/repositories/lead.repository.spec.ts
@@ -51,4 +51,21 @@ describe('LeadRepository', () => {
     expect(lead.status).toBe('INTERESTED')
     expect(lead.created_at).toEqual(new Date())
   })
+
+  test('should get lead by email', async () => {
+    const sut = makeSut()
+
+    await leadCollection.insertOne({
+      id: crypto.randomUUID(),
+      name: 'Any Name',
+      email: 'anyEmail@email.com',
+      status: 'INTERESTED',
+      created_at: new Date()
+    })
+
+    const lead = await sut.getByEmail('anyEmail@email.com')
+
+    expect(lead).toBeTruthy()
+    expect(lead.email).toBe('anyEmail@email.com')
+  })
 })
--- src/infra/database/repositories/lead.repository.ts
+++ src/infra/database/repositories/lead.repository.ts
@@ -1,9 +1,16 @@
+import { GetLeadByEmailRepositoryInterface } from '@/domain/repositories/get-lead-by-email.repository.interface'
 import { InputLead, SaveLeadRepositoryInterface } from '@/domain/repositories/save-lead.repository.interface'
 import { MongoHelper } from '../helpers/mongo.helper'
 
-export class LeadRepository implements SaveLeadRepositoryInterface {
+export class LeadRepository implements SaveLeadRepositoryInterface, GetLeadByEmailRepositoryInterface {
   async save (input: InputLead): Promise<void> {
     const leadCollection = await MongoHelper.getCollection('leads')
     await leadCollection.insertOne(input)
+  }
+
+  async getByEmail (email: string): Promise<any> {
+    const leadCollection = await MongoHelper.getCollection('leads')
+    const lead = await leadCollection.findOne({ email })
+    return lead || null
   }
 }
