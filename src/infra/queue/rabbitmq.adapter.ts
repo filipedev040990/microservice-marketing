5dad63dd17eef34eddd35d9692ac479e350bf1bf
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-19 07:00:49 -0300

    feat: implements rabbitmq adapter

---
+++ src/infra/queue/rabbitmq.adapter.ts
@@ -0,0 +1,33 @@
+import { Connection, Channel, connect } from 'amqplib'
+import { QueueInterface } from '@/domain/queue/queue.interface'
+
+export class RabbitmqAdapter implements QueueInterface {
+  private connection: Connection
+  private channel: Channel
+
+  constructor (private readonly uri: string) {}
+
+  async start (): Promise<void> {
+    this.connection = await connect(this.uri)
+    this.channel = await this.connection.createChannel()
+  }
+
+  async consume (queue: string, callback: Function): Promise<any> {
+    await this.channel.assertQueue(queue, { durable: true })
+    await this.channel.consume(queue, async (message: any) => {
+      if (message) {
+        const input = JSON.parse(message.context.toString())
+        await callback(input)
+        this.channel.ack(message)
+      }
+    })
+  }
+
+  async publish (exchange: string, routingKey: string, message: string): Promise<boolean> {
+    return await this.channel.publish(exchange, routingKey, Buffer.from(message))
+  }
+
+  async close (): Promise<void> {
+    await this.channel.close()
+  }
+}
